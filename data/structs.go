fa170970252a0184e934a9d4a1549efe331ae1a4
b <b@b.local>
2023-04-26 19:30:23 +0100

    Recipe

---
+++ data/structs.go
@@ -0,0 +1,13 @@
+package data
+
+type Recipe struct {
+	Id          string            `json:"Id"`
+	RecipeName  string            `json:"Recipe Name"`
+	Source      string            `json:"Source"`
+	PrepTime    string            `json:"Prep Time"`
+	CookTime    string            `json:"Cook Time"`
+	ServingSize int               `json:"Serving Size"`
+	Ingredients map[string]string `json:"Ingredients"`
+	Directions  map[int]string    `json:"Directions"`
+	Tags        []string          `json:"Tags"`
+}
